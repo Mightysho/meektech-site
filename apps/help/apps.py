2e99abbb7b8335b862182e8331aae7732928b946
Mightysho <mightysho4sure2014@gmail.com>
2026-03-23 07:29:27 +0100

    added FAQ apps

---
+++ apps/help/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.help'
+    verbose_name = "Help (Footer Section)"
