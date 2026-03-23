4c243e5b3a0b22cce133b820b22aceb3520e8cc9
Mightysho <mightysho4sure2014@gmail.com>
2026-03-23 06:23:51 +0100

    added FAQ admin

---
+++ apps/help/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Faq
+
+
+@admin.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "Question",
+        "Answer",
+    )
+    search_fields = ("Question", "Answer",)
+
+
