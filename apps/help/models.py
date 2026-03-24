5a8a584f7a65bf7a3bcda85863ec186c6eed7abc
Mightysho <mightysho4sure2014@gmail.com>
2026-03-24 17:07:37 +0100

    observed faq ordering

--- apps/help/models.py
+++ apps/help/models.py
@@ -7,5 +7,11 @@ class Faq(models.Model):
     Question = models.CharField(max_length=100, blank=True, null=True)
     Answer = models.TextField(blank=True, null=True)
 
+
+    class Meta:
+        # ordering = ["-created_at"]
+        verbose_name_plural = "Frequently Asked Question"
+
+
     def __str__(self):
         return f"{self.Question}"
