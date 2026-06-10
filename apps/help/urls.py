a844d37cdd1edd71cb6060d05d2b2abcff14a389
Mightysho <mightysho4sure2014@gmail.com>
2026-06-10 19:00:15 +0100

    modification changes

--- apps/help/urls.py
+++ apps/help/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("faq/", views.faqfooter, name="faq"),
+    path("ourterms/", views.faqfooter, name="our-terms"),
+    path("privacypolicy/", views.faqfooter, name="privacy-policy"),
 ]
-
-
 
 
--- apps/help/views.py
+++ apps/help/views.py
@@ -5,4 +5,11 @@ from .models import Faq
 def faqfooter(request):
     frequently = Faq.objects.all()
     return render(request, "help/faq.html", {"frequently": frequently})
+    
 
+def ourterms(request):
+    return render(request, "help/our-terms.html")
+    
+def privacypolicy(request):
+    return render(request, "help/privacy-policy.html")
+    
