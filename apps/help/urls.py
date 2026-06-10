6f9f488483a95884ac0a8d86a211f8cf47a95093
Mightysho <mightysho4sure2014@gmail.com>
2026-06-10 20:24:46 +0100

    commit updates

--- apps/help/urls.py
+++ apps/help/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("faq/", views.faqfooter, name="faq"),
-    path("ourterms/", views.faqfooter, name="our-terms"),
-    path("privacypolicy/", views.faqfooter, name="privacy-policy"),
+    path("ourterms/", views.ourterms, name="our-terms"),
+    path("privacypolicy/", views.privacypolicy, name="privacy-policy"),
 ]
 
 
