56e57d6cace08a606515858d5865ad674aea011f
Mightysho <mightysho4sure2014@gmail.com>
2026-06-12 01:23:42 +0100

    added url updates

--- apps/help/urls.py
+++ apps/help/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("privacypolicy/", views.privacypolicy, name="privacy-policy"),
     path("chat/", views.chat_page, name="chat"),
     path("send-message/", views.send_message, name="send_message"),
+    path("activate-human/<int:session_id>/", views.activate_human_support, name="activate_human_support")
 ]
 
 
