88910eaa5dbca9fcd2cf0359676b1979e3e0f281
Mightysho <mightysho4sure2014@gmail.com>
2026-07-14 01:01:56 +0100

    modify a file

--- apps/help/admin.py
+++ apps/help/admin.py
@@ -10,24 +10,3 @@ class FaqAdmin(admin.ModelAdmin):
     )
     search_fields = ("Question", "Answer",)
 
-
-@admin.register(ChatSession)
-class ChatSessionAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "visitor_name",
-        "is_human_active",
-        "assigned_agent",
-        "created_at",
-    )
-
-
-@admin.register(ChatMessage)
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = (
-        "session",
-        "sender",
-        "message",
-        "created_at",
-    )
-
